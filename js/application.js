bdb4795eb09c528820c3537f21be4d3b8c558579
gao_haohao@126.com <gao_haohao@126.com>
2019-03-03 20:50:13 +0800

    应用详情页面开发

---
+++ js/application.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $.guide(
+        [
+            {
+                selector: '.sel',
+                content: 'STEP .1',
+                align: 'left'
+            },
+            {
+                selector: '.demo',
+                content: '',
+                align: 'left'
+            }
+        ]
+    );
+});
